aa25e08d907f0a540d7ee23b64669e07d59b7ea7
Jerry Liu <jerry839_liu@zenitron.com.tw>
2024-10-15 09:21:56 +0800

    轉換MongoDB

--- routes/auth_route.js
+++ routes/auth_route.js
@@ -1,78 +1,61 @@
 require("dotenv").config();
+
 const router = require("express").Router();
-const Database = require("../models/database2");
-const QRCode = require("qrcode");
+const User = require("../models/").user;
+const Record = require("../models/").record;
 const nodemailer = require("nodemailer");
 const fs = require("fs");
 const path = require("path");
 const cloudinary = require("cloudinary").v2;
-
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+require("../config/passport")(passport);
 let file_name = "";
+
+router.get("/record", async (req, res) => {
+  let record = await Record.find();
+  res.send(record);
+});
 
 router.post("/login", async (req, res) => {
   let { password } = req.body;
   try {
-    if (Database.connect()) {
-      //若資料庫連接成功判斷輸入的識別碼是否有其名單
+    let foundUser = await User.findOne({ password });
 
-      let result = await Database.getAccount(password);
-      if (result.rows.length > 0) {
-        res.send(result.rows[0]);
-        return result.rows[0];
-      } else {
-        // return
-        return res.send(result.rows[0]);
-      }
+    if (foundUser) {
+      const tokenObject = { _id: password };
+      const token = jwt.sign(tokenObject, process.env.PASSPORT_SECRET);
+      return res.send({ data: foundUser, token: "JWT " + token });
     } else {
-      res.status(500).send("資料庫連接失敗");
+      return res.status(401).send("識別碼輸入錯誤");
     }
   } catch (e) {
     console.log(e);
   }
 });
 
-router.post("/login_person", async (req, res) => {
-  let { password } = req.body;
-  try {
-    if (Database.connect()) {
-      //若資料庫連接成功判斷輸入的識別碼是否有其名單
-
-      let result = await Database.getAccount_person(password);
-      if (result.rows.length > 0) {
-        res.send(result.rows[0]);
-        return result.rows[0];
-      } else {
-        // return
-        return res.send(result.rows[0]);
-      }
-    } else {
-      res.status(500).send("資料庫連接失敗");
+router.post(
+  "/update_data",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    let { password, ticket_left } = req.body;
+    try {
+      let result = await User.findOneAndUpdate(
+        { password },
+        { ticket_rest: ticket_left },
+        {
+          new: true,
+          runValidators: true,
+        }
+      ).exec();
+      return result ? res.send(result) : res.status(500).send("更新資料庫失敗");
+    } catch (e) {
+      console.log(e);
     }
-  } catch (e) {
-    console.log(e);
   }
-});
+);
 
-router.post("/update_data", async (req, res) => {
-  let { password, ticket_left } = req.body;
-
-  try {
-    if (Database.connect()) {
-      //若資料庫連接成功判斷輸入的識別碼是否有其名單
-      let result = await Database.updateData(password, ticket_left);
-      if (result) {
-        return res.send(result);
-      } else {
-        res.status(500).send("更新資料庫失敗");
-      }
-    } else {
-      res.status(500).send("資料庫連接失敗");
-    }
-  } catch (e) {
-    console.log(e);
-  }
-});
-
+//QRCODE轉檔
 cloudinary.config({
   cloud_name: process.env.cloud_name,
   api_key: process.env.api_key,
@@ -111,8 +94,16 @@ router.post("/user-send-email", async (req, res) => {
     let formattedString = cur_time.toLocaleString();
     cur_time = formattedString.slice(0, 10).trim(); //領票日期
 
-    const { qrCodeUrl, names, seat, username, numbers, emails } = req.body;
-    console.log("url....test...", qrCodeUrl);
+    const {
+      qrCodeUrl,
+      company,
+      names,
+      seat,
+      username,
+      numbers,
+      emails,
+      ticketNum,
+    } = req.body;
     let domain = emails.split("@")[1]; //取得domain
 
     // 保存多個 QR 碼並上傳到 Cloudinary
@@ -147,70 +138,138 @@ router.post("/user-send-email", async (req, res) => {
     });
 
     let mailOptions = "";
-    if (domain == "gmail.com") {
-      mailOptions = {
-        from: '"<qrcode0908@gmail.com>"',
-        to: emails,
-        subject: "<<接棒未來，揮出夢想>>取票成功通知",
-        html: `
-          <p>${username} 您好，</p>
-          <p style="color:red">取票成功!當天出示下方QRCODE即可入場！期待您一同共襄盛舉！</p>
-          <img src="${qrCodeUrlOnCloudinary}" alt="QR Code" />
+
+    //領票數量兩張以上，以附檔方式寄送
+    if (numbers > 1) {
+      if (domain == "gmail.com") {
+        mailOptions = {
+          from: '"活動通知-入場電子票券" <qrcode0908@gmail.com>',
+          to: emails,
+          subject: "<<接棒未來，揮出夢想>>取票成功通知",
+          html: `
+          <p>${username} 您好，<span style="color:red">您已取票成功！</span></p>
+          <p>附件為您所領取的入場電子票券QRCODE，</p>
+          <p style="color:red">再請協助將入場電子票券QRCODE轉發給其它出席人員，每張入場電子票券QRCODE僅限1人使用</p>
+          <p>當天於驗票口出示即可入場，期待您一同前來共襄盛舉!</p>
           <p>活動名稱：接棒未來，揮出夢想</p>
           <p>活動地點：台北大巨蛋-台北市信義區忠孝東路四段515號</p>
           <p>活動日期：2024年11月23日(六)</p>
           <p>活動時間：13:00~15:30</p>
-          <p>公司名稱：${names}</p>
+          <p>公司名稱/捐款人名稱：${names}</p>
           <p>領票人姓名：${username}</p>
           <p>座位區域：${seat}</p>
-          <p>取票數量：${numbers}</p>
-          <p style="color:blue; font-size:14px;">***系統操作或票券取得相關問題，請洽客服專線(02)2792-8788#502</p>
-          <p style="color:blue; font-size:14px;">***服務時間：週一到週五 09:00~18:00</p>
+          <p>票券號碼：${ticketNum + 1}~${ticketNum + numbers}</p>
+         
+          <p style="color:blue; font-size:14px;">如有票券取得之相關問題，請隨時與我們聯繫，謝謝</p>
+          <p style="color:blue; font-size:14px;">客服電話：(02)2792-8788#502</p>
+          <p style="color:blue; font-size:14px;">客服信箱：xgen.org.tw@gmail.com</p>
+          <p style="color:blue; font-size:14px;">服務時間：週一到週五 09:00~18:00</p>
         `,
-        attachments: attachments,
-      };
-    } else {
-      mailOptions = {
-        from: '"活動通知-入場電子票券" <qrcode0908@gmail.com>',
-        to: emails,
-        subject: "<<接棒未來，揮出夢想>>取票成功通知",
-        html: `
-        <p>${username} 您好，</p>
-        <p style="color:red">取票成功!當天出示下方QRCODE即可入場！期待您一同共襄盛舉！</p>
-        <img src="${qrCodeUrl}" alt="QR Code" />
-        <p>活動名稱：接棒未來，揮出夢想</p>
-        <p>活動地點：台北大巨蛋-台北市信義區忠孝東路四段515號</p>
-        <p>活動日期：2024年11月23日(六)</p>
-        <p>活動時間：13:00~15:30</p>
-        <p>公司名稱：${names}</p>
-        <p>領票人姓名：${username}</p>
-        <p>座位區域：${seat}</p>
-        <p>取票數量：${numbers}</p>
-        <p style="color:blue; font-size:14px;">***系統操作或票券取得相關問題，請洽客服專線(02)2792-8788#502</p>
-        <p style="color:blue; font-size:14px;">***服務時間：週一到週五 09:00~18:00</p>
-      `,
-        attachments: attachments,
-      };
+          attachments: attachments,
+        };
+      } else {
+        mailOptions = {
+          from: '"活動通知-入場電子票券" <qrcode0908@gmail.com>',
+          to: emails,
+          subject: "<<接棒未來，揮出夢想>>取票成功通知",
+          html: `
+          <p>${username} 您好，<span style="color:red">您已取票成功！</span></p>
+          <p>附件為您所領取的入場電子票券QRCODE，</p>
+          <p style="color:red">再請協助將入場電子票券QRCODE轉發給其它出席人員，每張入場電子票券QRCODE僅限1人使用</p>
+          <p>當天於驗票口出示即可入場，期待您一同前來共襄盛舉!</p>
+          <p>活動名稱：接棒未來，揮出夢想</p>
+          <p>活動地點：台北大巨蛋-台北市信義區忠孝東路四段515號</p>
+          <p>活動日期：2024年11月23日(六)</p>
+          <p>活動時間：13:00~15:30</p>
+          <p>公司名稱/捐款人名稱：${names}</p>
+          <p>領票人姓名：${username}</p>
+          <p>座位區域：${seat}</p>
+          <p>票券號碼：${ticketNum + 1}~${ticketNum + numbers}</p>
+          <p style="color:blue; font-size:14px;">如有票券取得之相關問題，請隨時與我們聯繫，謝謝</p>
+          <p style="color:blue; font-size:14px;">客服電話：(02)2792-8788#502</p>
+          <p style="color:blue; font-size:14px;">客服信箱：xgen.org.tw@gmail.com</p>
+          <p style="color:blue; font-size:14px;">服務時間：週一到週五 09:00~18:00</p>
+        `,
+          attachments: attachments,
+        };
+      }
+    }
+    // 數量單張直接以圖片呈現
+    else {
+      if (domain == "gmail.com") {
+        mailOptions = {
+          from: '"<qrcode0908@gmail.com>"',
+          to: emails,
+          subject: "<<接棒未來，揮出夢想>>取票成功通知",
+          html: `
+            <p>${username} 您好，<span style="color:red">您已取票成功！</span></p>
+            <p>下方為您的入場票券QRCODE，<span style="color:red">每張入場電子票券QRCODE僅限1人使用</span></p>
+            <p>當天於驗票口出示即可入場，期待您一同前來共襄盛舉!</p>
+            <p>入場票券QRCODE</p>
+            <img src="${qrCodeUrlOnCloudinary}" alt="QR Code" />
+            <p>活動名稱：接棒未來，揮出夢想</p>
+            <p>活動地點：台北大巨蛋-台北市信義區忠孝東路四段515號</p>
+            <p>活動日期：2024年11月23日(六)</p>
+            <p>活動時間：13:00~15:30</p>
+            <p>公司名稱/捐款人名稱：${names}</p>
+            <p>領票人姓名：${username}</p>
+            <p>座位區域：${seat}</p>
+            <p>票券號碼：${ticketNum + 1}</p>
+            
+            <p style="color:blue; font-size:14px;">如有票券取得之相關問題，請隨時與我們聯繫，謝謝</p>
+            <p style="color:blue; font-size:14px;">客服電話：(02)2792-8788#502</p>
+            <p style="color:blue; font-size:14px;">客服信箱：xgen.org.tw@gmail.com</p>
+            <p style="color:blue; font-size:14px;">服務時間：週一到週五 09:00~18:00</p>
+          `,
+        };
+      } else {
+        mailOptions = {
+          from: '"活動通知-入場電子票券" <qrcode0908@gmail.com>',
+          to: emails,
+          subject: "<<接棒未來，揮出夢想>>取票成功通知",
+          html: `
+          <p>${username} 您好，<span style="color:red">您已取票成功！</span></p>
+          <p>下方為您的入場票券QRCODE，<span style="color:red">每張入場電子票券QRCODE僅限1人使用</span></p>
+          <p>當天於驗票口出示即可入場，期待您一同前來共襄盛舉!</p>
+          <p>入場票券QRCODE</p>
+          <img src="${qrCodeUrl}" alt="QR Code" />
+          <p>活動名稱：接棒未來，揮出夢想</p>
+          <p>活動地點：台北大巨蛋-台北市信義區忠孝東路四段515號</p>
+          <p>活動日期：2024年11月23日(六)</p>
+          <p>活動時間：13:00~15:30</p>
+          <p>公司名稱/捐款人名稱：${names}</p>
+          <p>領票人姓名：${username}</p>
+          <p>座位區域：${seat}</p>
+          <p>票券號碼：${ticketNum + 1}</p>
+          
+          <p style="color:blue; font-size:14px;">如有票券取得之相關問題，請隨時與我們聯繫，謝謝</p>
+          <p style="color:blue; font-size:14px;">客服電話：(02)2792-8788#502</p>
+          <p style="color:blue; font-size:14px;">客服信箱：xgen.org.tw@gmail.com</p>
+          <p style="color:blue; font-size:14px;">服務時間：週一到週五 09:00~18:00</p>
+        `,
+        };
+      }
     }
 
     // 設定郵件內容
-
     // 發送郵件
     try {
       let send_result = await transporter.sendMail(mailOptions);
       if (send_result) {
         res.status(200).json({ message: "Email sent successfully" });
         //將領取票券資料存入DB
-        if (Database.connect()) {
-          Database.insertData(
-            cur_time,
-            names,
-            username,
-            numbers,
-            emails,
-            file_name
-          );
-        }
+
+        //insertOne函式需要使用collection.
+        const saveResult = await Record.collection.insertOne({
+          get_ticket_date: cur_time,
+          donor: names,
+          taker: username,
+          ticket_id: ticketNum + numbers,
+          ticket_count: numbers,
+          seat: seat,
+          email: emails,
+          url: file_name,
+        });
       }
     } catch (error) {
       console.error("Error sending email:", error);
@@ -221,99 +280,21 @@ router.post("/user-send-email", async (req, res) => {
   }
 });
 
-router.post("/user-send-email-person", async (req, res) => {
-  if (req.method === "POST") {
-    //日期格式轉換
-    let cur_time = new Date();
-    let formattedString = cur_time.toLocaleString();
-    cur_time = formattedString.slice(0, 10).trim(); //領票日期
+router.get("/get-ticket-id", async (req, res) => {
+  try {
+    //取得資料中最小的排序，資料會以array方式return
+    let foundUser = await Record.aggregate([
+      { $sort: { ticket_id: -1 } },
+      { $limit: 1 },
+    ]);
 
-    const { qrCodeUrl, names, seat, username, numbers, emails } = req.body;
-    let domain = emails.split("@")[1]; //取得domain
-
-    // 保存base64圖片到伺服器
-    const filePath = saveQRCodeToFile(qrCodeUrl);
-
-    // 上傳到 Cloudinary 並獲取公開的 URL
-    const qrCodeUrlOnCloudinary = await uploadQRCodeToCloudinary(filePath);
-
-    // 設定郵件傳輸服務
-    let transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.user,
-        pass: process.env.password,
-      },
-    });
-
-    let mailOptions = "";
-    if (domain == "gmail.com") {
-      mailOptions = {
-        from: '"<qrcode0908@gmail.com>"',
-        to: emails,
-        subject: "<<接棒未來，揮出夢想>>取票成功通知",
-        html: `
-          <p>${username} 您好，</p>
-          <p style="color:red">取票成功!當天出示下方QRCODE即可入場！期待您一同共襄盛舉！</p>
-          <img src="${qrCodeUrlOnCloudinary}" alt="QR Code" />
-          <p>活動名稱：接棒未來，揮出夢想</p>
-          <p>活動地點：台北大巨蛋-台北市信義區忠孝東路四段515號</p>
-          <p>活動日期：2024年11月23日(六)</p>
-          <p>活動時間：13:00~15:30</p>
-          <p>領票人姓名：${username}</p>
-          <p>座位區域：${seat}</p>
-          <p>取票數量：${numbers}</p>
-          <p style="color:blue; font-size:14px;">***系統操作或票券取得相關問題，請洽客服專線(02)2792-8788#502</p>
-          <p style="color:blue; font-size:14px;">***服務時間：週一到週五 09:00~18:00</p>
-        `,
-      };
+    if (foundUser.length == 0) {
+      return res.json("20240000");
     } else {
-      mailOptions = {
-        from: '"活動通知-入場電子票券" <qrcode0908@gmail.com>',
-        to: emails,
-        subject: "<<接棒未來，揮出夢想>>取票成功通知",
-        html: `
-        <p>${username} 您好，</p>
-        <p style="color:red">取票成功!當天出示下方QRCODE即可入場！<span style="color:purple">期待您一同共襄盛舉！</span></p>
-        <img src="${qrCodeUrl}" alt="QR Code" />
-        <p>活動名稱：接棒未來，揮出夢想</p>
-        <p>活動地點：台北大巨蛋-台北市信義區忠孝東路四段515號</p>
-        <p>活動日期：2024年11月23日(六)</p>
-        <p>活動時間：13:00~15:30</p>
-        <p>領票人姓名：${username}</p>
-        <p>座位區域：${seat}</p>
-        <p>取票數量：${numbers}</p>
-        <p style="color:blue; font-size:14px;">***系統操作或票券取得相關問題，請洽客服專線(02)2792-8788#502</p>
-        <p style="color:blue; font-size:14px;">***服務時間：週一到週五 09:00~18:00</p>
-      `,
-      };
+      return res.json(foundUser[0].ticket_id);
     }
-
-    // 設定郵件內容
-
-    // 發送郵件
-    try {
-      let send_result = await transporter.sendMail(mailOptions);
-      if (send_result) {
-        res.status(200).json({ message: "Email sent successfully" });
-        //將領取票券資料存入DB
-        if (Database.connect()) {
-          Database.insertData(
-            cur_time,
-            names,
-            username,
-            numbers,
-            emails,
-            file_name
-          );
-        }
-      }
-    } catch (error) {
-      console.error("Error sending email:", error);
-      return res.status(500).json({ error: "Failed to send email" });
-    }
-  } else {
-    return res.status(405).json({ message: "Method not allowed" });
+  } catch (e) {
+    console.log(e);
   }
 });
 
