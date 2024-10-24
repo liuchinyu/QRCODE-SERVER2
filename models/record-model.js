e0dccd572a595b852cb5bf9210f1bdb7250ac635
Jerry Liu <jerry839_liu@zenitron.com.tw>
2024-10-24 13:22:38 +0800

    領票記錄新增剩餘票券

--- models/record-model.js
+++ models/record-model.js
@@ -8,6 +8,7 @@ const recordSchema = new Schema({
   ticket_id: { type: String, required: true },
   ticket_count: { type: Number, required: true },
   ticket_kid: { type: Number, required: true },
+  ticket_left: { type: Number, required: true },
   seat: { type: String, required: true },
   email: { type: String, required: true },
   url: { type: String, required: true },
